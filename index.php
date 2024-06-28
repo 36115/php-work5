e052dade3af77894236998cc1b9790bf24ba5df9
36115 <138181602+36115@users.noreply.github.com>
2024-06-28 06:48:18 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>สรวิชญ์-610-11</title>
+</head>
+<body>
+    <?php
+
+    $i = 11;
+    $num = $i - 1;
+
+    if ($i > 1) {
+        $j = 1;
+        while ($i <= 40) {
+            echo "เลขที่ $i<br>";
+            $i++;
+        }
+        while ($j <= $num) {
+            echo "เลขที่ $j<br>";
+            $j++;
+        }
+    }
+
+    ?>
+</body>
+</html>
